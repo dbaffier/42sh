790c16eabb28a50d114b614878162b8b1df26be1
Dorian BAFFIER <dbaffier@e2r2p21.42.fr>
2019-05-09 09:21:54 +0200

    {42sh to _git}

---
+++ src/expansion/expand_dollar_get_action.c
@@ -0,0 +1,93 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   expand_dollar_get_action.c                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gbourgeo <gbourgeo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/12/28 06:50:11 by gbourgeo          #+#    #+#             */
+/*   Updated: 2019/04/28 21:57:15 by gbourgeo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include "expansion_is.h"
+#include "expansion_action.h"
+#include "expansion_errors.h"
+#include "expansion_dollar.h"
+
+static int		action_percent(t_exp *param, t_ret *ret, int value)
+{
+	(void)param;
+	++ret->i;
+	ret->action = REMOVE_SMALLEST_SUFFIX_PATTERN;
+	value = ret->head[ret->brace + ret->hash + ret->i];
+	if (value == '%')
+	{
+		ret->action = REMOVE_LARGEST_SUFFIX_PATTERN;
+		++ret->i;
+	}
+	return (ERR_NONE);
+}
+
+static int		action_hash(t_exp *param, t_ret *ret, int value)
+{
+	(void)param;
+	++ret->i;
+	ret->action = REMOVE_SMALLEST_PREFIX_PATTERN;
+	value = ret->head[ret->brace + ret->hash + ret->i];
+	if (value == '#')
+	{
+		ret->action = REMOVE_LARGEST_PREFIX_PATTERN;
+		++ret->i;
+	}
+	return (ERR_NONE);
+}
+
+static int		action_values(t_exp *param, t_ret *ret, int value)
+{
+	(void)param;
+	if (value == '-')
+		ret->action |= ACT_NULL_SUBST;
+	else if (value == '=')
+		ret->action |= ACT_NULL_ASSIGN;
+	else if (value == '?')
+		ret->action |= ACT_NULL_ERROR;
+	else if (value == '+')
+		ret->action |= ACT_NONNULL_SUBST;
+	++ret->i;
+	return (ERR_NONE);
+}
+
+static int		action_colon(t_exp *param, t_ret *ret, int value)
+{
+	ret->action = COLON_ACTION;
+	value = ret->head[++ret->i];
+	if (ft_strchr("-=?+", value))
+		return (action_values(param, ret, value));
+	return (expand_arithmetic(param, ret));
+}
+
+int				expand_dollar_get_action(t_ret *ret, t_exp *param)
+{
+	static t_action	action[] = {
+		{ ":", action_colon }, { "-=?+", action_values },
+		{ "#", action_hash }, { "%", action_percent },
+	};
+	size_t			i;
+	int				value;
+
+	i = 0;
+	value = ret->head[ret->i];
+	if (!ret->brace
+	|| is_expand_null(ret)
+	|| value == '}')
+		return (ERR_NONE);
+	while (i < sizeof(action) / sizeof(action[0]))
+	{
+		if (ft_strchr(action[i].value, value))
+			return (action[i].handler(param, ret, value));
+		i++;
+	}
+	return (ERR_NONE);
+}
